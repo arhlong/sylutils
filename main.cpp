65a3f0d06fb584cdf40da404c292c84c8898010f
syl <yuelong.su@quantum-info.com>
2016-01-19 09:41:52 +0800

    commit the source files.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+int main()
+{
+	std::shared_ptr<std::string> p1(new std::string("hello world!"));
+	std::shared_ptr<std::string> p2 = p1;
+
+	std::unique_ptr<std::string> p3(new std::string("hello girl!"));
+
+	std::auto_ptr<std::string> p4(new std::string("hello arhlong!"));
+
+
+
+    return 0;
+}
